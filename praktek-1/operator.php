66f97e4a485d69564ab6cf901ab2f587a4b8173d
tamami <tamami.oka@gmail.comgit>
2018-02-21 17:39:26 +0700

    nambah file operator.php

---
+++ praktek-1/operator.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 5;
+$b = 3;
+$c = 5;
+$d = "5";
+
+echo "$a + $b = "; echo $a + $b; echo "<br>";
+echo "$a / $b = "; echo $a / $b; echo "<br>";
+echo "$a > $b : "; echo $a > $b; echo "<br>";
+echo "$a == $c : "; var_dump($a == $c); echo "<br>";
+echo "$a == $d : "; var_dump($a == $d); echo "<br>";
+echo "$a === $d : "; var_dump($a === $d); echo "<br>";
+
+echo $a++; echo "<br>";
+echo ++$a; echo "<br>";
+
+
+?>
